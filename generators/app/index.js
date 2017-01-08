842f88a17e57e9b5af8f96f6a27a3447852f5d20
Oli Gibson <oligibson1@gmail.com>
2017-01-08 12:46:30 +0000

    Made Server Optional

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,17 +6,19 @@ module.exports = bitmate.Base.extend({
   },
 
   composing() {
-    this.composeWith(`bitmate-${this.props.server}`, {
-      options: {
-        framework: this.props.server,
-        client: this.props.client,
-        runner: this.props.runner,
-        skipInstall: this.props.skipInstall,
-        skipCache: this.props.skipCache
-      }
-    }, {
-      local: require.resolve(`generator-bitmate-${this.props.server}/generators/app`)
-    });
+    if (this.props.server !== 'none') {
+      this.composeWith(`bitmate-${this.props.server}`, {
+        options: {
+          framework: this.props.server,
+          client: this.props.client,
+          runner: this.props.runner,
+          skipInstall: this.props.skipInstall,
+          skipCache: this.props.skipCache
+        }
+      }, {
+        local: require.resolve(`generator-bitmate-${this.props.server}/generators/app`)
+      });
+    }
 
     if (this.props.client !== 'none') {
       this.composeWith(`bitmate-${this.props.client}`, {
